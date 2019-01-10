c5ee176b9be2501ca51b03dd1cbe8dd369cc3630
wangxu <593464677>
2019-01-10 17:20:05 +0800

    Initial commit

---
+++ src/test/java/com/example/springcould_demo/SpringcouldDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.springcould_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcouldDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
